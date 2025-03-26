88f476eada9b9e1f9ec6ad3e68e1fa952da69abd
AdamHale88 <g3nomezer0@gmail.com>
2025-03-26 00:27:53 -0400

    removed unneeded file, mid-day saving commit

--- src/main_game.py
+++ src/main_game.py
@@ -1,23 +1,43 @@
-import math_functions 
-dash = ("_")*40
-playerSelection = ""
-print()
-print(dash)
-print("         Fates Gamble            ")
-print()
-print(dash)
-print()
-print("Total Shots: ",math_functions.totalShotsFired, " | " "Cash: $",math_functions.userScore)
-print()
-print(dash)
-print()
-print("( T ): Trigger")
-print()
-print("( P ): Pass")
-print()
-print("( X ): Easy Way Out")
-print() 
-print()
-playerSelection = input("Selection:  ")
-print()
-           
+import math_functions
+import os
+def main ():
+    dash = ("_")*40
+    playerSelection = ""
+    while math_functions.totalShotsFired != 5 and playerSelection !='X':
+        try:
+            print()
+            print(dash)
+            print("         Fates Gamble            ")
+            print()
+            print(dash)
+            print()
+            print("Total Shots: ",math_functions.totalShotsFired, " | " "Cash: $",math_functions.playersScore)
+            print()
+            print(dash)
+            print()
+            print("( T ): Trigger")
+            print()
+            print("( P ): Pass")
+            print()
+            print("( X ): Easy Way Out")
+            print() 
+            print()
+            playerSelection = input("Selection:  ")
+            print()
+        finally:
+            if math_functions.totalShotsFired == 5:
+                print()          
+                print("New game ?")
+                print()
+                print()
+                playerSelection = input( "(Y) for Yes or (N) for No :      ")
+                print()
+                print()
+                if playerSelection.upper() == ("Y"):
+                        math_functions.totalShotsFired = 0
+                        math_functions.playerScore = 0
+                else:
+                    playerSelection = "X"
+            else:
+                None  
+main()
---
+++ src/player_module.py
@@ -0,0 +1,2 @@
+import random
+playersShot = random.randint(1,5)
